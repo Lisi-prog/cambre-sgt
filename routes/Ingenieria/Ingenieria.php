59667b86e49a72180214b3b26024820195d94015
Lisi-prog <80710956+Lisi-prog@users.noreply.github.com>
2024-02-15 14:39:39 -0300

    Update Ingenieria.php

--- routes/Ingenieria/Ingenieria.php
+++ routes/Ingenieria/Ingenieria.php
@@ -72,7 +72,7 @@ Route::group(['middleware' => ['auth','role_or_permission:ADMIN|SUPERVISOR']], f
     Route::resource('activos', ActivoController::class);
 });
 
-Route::group(['middleware' => ['auth','role_or_permission:ADMIN|SUPERVISOR']], function () {
+Route::group(['middleware' => ['auth','role_or_permission:ADMIN|SUPERVISOR|EMPLEADO']], function () {
     Route::resource('s_s_i', ServicioDeIngenieriaController::class);
     Route::get('r_i/evaluar/{id}', [RequerimientoDeIngenieriaController::class, 'evaluar'])->name('ri.evaluar');
     Route::post('r_i/evaluar/aceptar/{id}', [ProyectoController::class, 'aceptar_solicitud'])->name('solicitud.aceptar');
