70e878af8b1fdbd6ce2dac23dc5e51b8cd6e0a2c
JDCaruza <j.d.caruza@gmail.com>
2024-02-22 17:10:47 -0300

    vista desc largas para act

--- public/js/Ingenieria/Servicios/Proyectos/modal/gestionar.js
+++ public/js/Ingenieria/Servicios/Proyectos/modal/gestionar.js
@@ -23,10 +23,10 @@ function mostrarActProyecto(id){
             html_act += `<tr>
                             <td class="text-center">`+element.codigo+`</td>
                             <td class="text-center">`+element.fecha_carga+`</td>
-                            <td class="text-center">`+element.descripcion+`</td>
+                            <td class="text-center"><abbr title="`+element.descripcion+`" style="text-decoration:none; font-variant: none;">`+element.descripcion.slice(0, 25)+`<i class="fas fa-eye"></i></abbr></td>
                             <td class="text-center">`+element.fecha_limite+`</td>
                             <td class="text-center">`+element.estado+`</td>
-                            <td class="text-center">`+element.responsable+`</td>
+                            <td class="text-center">`+element.responsable+`</td>    
                             </tr>`
         });
         renglones_actualizacion.innerHTML = html_act;
@@ -61,7 +61,7 @@ function mostrarActEtapa(id){
                 html_act += `<tr>
                                 <td class="text-center">`+element.codigo+`</td>
                                 <td class="text-center">`+element.fecha_carga+`</td>
-                                <td class="text-center">`+element.descripcion+`</td>
+                                <td class="text-center"><abbr title="`+element.descripcion+`" style="text-decoration:none; font-variant: none;">`+element.descripcion.slice(0, 25)+`<i class="fas fa-eye"></i></abbr></td>
                                 <td class="text-center">`+element.fecha_limite+`</td>
                                 <td class="text-center">`+element.estado+`</td>
                                 <td class="text-center">`+element.responsable+`</td>
