9ea7238826b5bc3a429503bdfb5fdeb5bc2f7170
Lisi-prog <80710956+Lisi-prog@users.noreply.github.com>
2024-02-22 09:39:29 -0300

    Update menu.blade.php

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -1,103 +1,8 @@
-<li class="side-menus {{ Request::is('*') ? 'active' : '' }}">
-    <a class="nav-link" href="/">
-        <i class=" fas fa-building"></i><span>Cambre S.A.</span>
-    </a>
-</li>
-
-    @can('VER-MENU-INFORMATICA')
-    <li class="dropdown ">
-        <a href="" class="nav-link has-dropdown ">
-            <i class="fas fa-server"></i><span>Informatica</span>
+    <li class="side-menus {{ Request::is('*') ? 'active' : '' }}">
+        <a class="nav-link" href="/">
+            <i class=" fas fa-building"></i><span>Cambre S.A.</span>
         </a>
-        <ul class="dropdown-menu border border-primary border-2 borde-menu" style="display: none;">
-            <li class="dropdown-title pt-3">
-                Informatica
-            </li>
-        
-            <li class="dropdown ">
-                <a href="" class="nav-link has-dropdown">
-                    <i class="fas fa-tools"></i><span>Herramientas</span>
-                </a>
-                
-                <ul class="dropdown-menu border border-primary border-2 borde-menu" style="display: none;">
-                    <li class="dropdown-title pt-3">
-                        Herramientas
-                    </li>
-                
-                    {{-- <li>
-                        <a class="nav-link" href="" title="Logs-app">
-                            <i class="fas fa-clipboard-list"style="font-size:1.2em; "></i><span>Logs-app</span>
-                        </a>
-                    </li> --}}
-            
-                    {{-- <li>
-                        <a class="nav-link" href="" title="Logs-audit">
-                            <i class="fas fa-clipboard-list"style="font-size:1.2em; "></i><span>Logs-audit</span>
-                        </a>
-                    </li> --}}
-                    
-                    <li>
-                        <a class="nav-link" href="{{route('phpmyinfo')}}" title="PHPinfo">
-                            <i class="fas fa-tools"style="font-size:1.2em; "></i><span>PHPinfo</span>
-                        </a>
-                    </li>
-                </ul>
-            </li>
-
-            <li>
-                <a class="nav-link" href="{{route('permisos.index')}}" title="Permisos">
-                    <i class="fas fa-server"style="font-size:1.2em; "></i><span>Permisos</span>
-                </a>
-            </li>
-
-            <li>
-                <a class="nav-link" href="{{route('roles.index')}}" title="Roles">
-                    <i class="fas fa-server"style="font-size:1.2em; "></i><span>Roles</span>
-                </a>
-            </li>
-
-            <li>
-                <a class="nav-link" href="{{route('usuarios.index')}}" title="Usuarios">
-                    <i class="fas fa-server"style="font-size:1.2em; "></i><span>Usuarios</span>
-                </a>
-            </li>
-            <li>
-                <a class="nav-link" href="{{route('tecnicos.index')}}" title="Tecnicos">
-                    <i class="fas fa-server"style="font-size:1.2em; "></i><span>Técnicos</span>
-                </a>
-            </li>
-        </ul>
     </li>
-    @endcan
-
-    @can('VER-MENU-INFORMATICA')
-    <li class="dropdown ">
-        <a href="" class="nav-link has-dropdown ">
-            <i class="fas fa-wrench"></i><span>Ingenieria</span>
-        </a>
-        <ul class="dropdown-menu border border-primary border-2 borde-menu" style="display: none;">
-            <li class="dropdown-title pt-3">
-                Ingenieria
-            </li>
-
-            @can('VER-MENU-MAQUINARIA')
-            <li>
-                <a class="nav-link" href="{{route('maquinarias.index')}}" title="Maquinaria">
-                    <i class="fas fa-wrench"style="font-size:1.2em; "></i><span>Maquinaria</span>
-                </a>
-            </li>
-            @endcan
-
-            @can('VER-MENU-ACTIVOS')
-            <li>
-                <a class="nav-link" href="{{route('activos.index')}}" title="Activos">
-                    <i class="fas fa-wrench"style="font-size:1.2em; "></i><span>Activos</span>
-                </a>
-            </li>
-            @endcan
-        </ul>
-    </li>
-    @endcan
 
     @can('VER-MENU-SOLICITUDES')
         <li class="dropdown ">
@@ -219,4 +124,99 @@
                 </li>
             </ul>
         </li>
+    @endcan
+
+    @can('VER-MENU-INFORMATICA')
+    <li class="dropdown ">
+        <a href="" class="nav-link has-dropdown ">
+            <i class="fas fa-server"></i><span>Informatica</span>
+        </a>
+        <ul class="dropdown-menu border border-primary border-2 borde-menu" style="display: none;">
+            <li class="dropdown-title pt-3">
+                Informatica
+            </li>
+        
+            <li class="dropdown ">
+                <a href="" class="nav-link has-dropdown">
+                    <i class="fas fa-tools"></i><span>Herramientas</span>
+                </a>
+                
+                <ul class="dropdown-menu border border-primary border-2 borde-menu" style="display: none;">
+                    <li class="dropdown-title pt-3">
+                        Herramientas
+                    </li>
+                
+                    {{-- <li>
+                        <a class="nav-link" href="" title="Logs-app">
+                            <i class="fas fa-clipboard-list"style="font-size:1.2em; "></i><span>Logs-app</span>
+                        </a>
+                    </li> --}}
+            
+                    {{-- <li>
+                        <a class="nav-link" href="" title="Logs-audit">
+                            <i class="fas fa-clipboard-list"style="font-size:1.2em; "></i><span>Logs-audit</span>
+                        </a>
+                    </li> --}}
+                    
+                    <li>
+                        <a class="nav-link" href="{{route('phpmyinfo')}}" title="PHPinfo">
+                            <i class="fas fa-tools"style="font-size:1.2em; "></i><span>PHPinfo</span>
+                        </a>
+                    </li>
+                </ul>
+            </li>
+
+            <li>
+                <a class="nav-link" href="{{route('permisos.index')}}" title="Permisos">
+                    <i class="fas fa-server"style="font-size:1.2em; "></i><span>Permisos</span>
+                </a>
+            </li>
+
+            <li>
+                <a class="nav-link" href="{{route('roles.index')}}" title="Roles">
+                    <i class="fas fa-server"style="font-size:1.2em; "></i><span>Roles</span>
+                </a>
+            </li>
+
+            <li>
+                <a class="nav-link" href="{{route('usuarios.index')}}" title="Usuarios">
+                    <i class="fas fa-server"style="font-size:1.2em; "></i><span>Usuarios</span>
+                </a>
+            </li>
+            <li>
+                <a class="nav-link" href="{{route('tecnicos.index')}}" title="Tecnicos">
+                    <i class="fas fa-server"style="font-size:1.2em; "></i><span>Técnicos</span>
+                </a>
+            </li>
+        </ul>
+    </li>
+    @endcan
+
+    @can('VER-MENU-INGENIERIA')
+    <li class="dropdown ">
+        <a href="" class="nav-link has-dropdown ">
+            <i class="fas fa-wrench"></i><span>Ingenieria</span>
+        </a>
+        <ul class="dropdown-menu border border-primary border-2 borde-menu" style="display: none;">
+            <li class="dropdown-title pt-3">
+                Ingenieria
+            </li>
+
+            @can('VER-MENU-MAQUINARIA')
+            <li>
+                <a class="nav-link" href="{{route('maquinarias.index')}}" title="Maquinaria">
+                    <i class="fas fa-wrench"style="font-size:1.2em; "></i><span>Maquinaria</span>
+                </a>
+            </li>
+            @endcan
+
+            @can('VER-MENU-ACTIVOS')
+            <li>
+                <a class="nav-link" href="{{route('activos.index')}}" title="Activos">
+                    <i class="fas fa-wrench"style="font-size:1.2em; "></i><span>Activos</span>
+                </a>
+            </li>
+            @endcan
+        </ul>
+    </li>
     @endcan
