3b61f2bd5496fa28e8abf5e8e32e6074ef4dc82e
Lisi-prog <80710956+Lisi-prog@users.noreply.github.com>
2024-03-06 18:38:36 -0300

    fix orden de estado al act serv

--- public/js/change-scripts.js
+++ public/js/change-scripts.js
@@ -19,4 +19,14 @@ $(document).ready(function () {
         backdrop: 'static',
         keyboard: true
         })
+    
+    /* $('.mcarga').on('hidden.bs.modal', function (e) {
+        $(this)
+            .find("input,textarea,select")
+                .val('')
+                .end()
+            .find("input[type=checkbox], input[type=radio]")
+                .prop("checked", "")
+                .end();
+        }) */
 });
