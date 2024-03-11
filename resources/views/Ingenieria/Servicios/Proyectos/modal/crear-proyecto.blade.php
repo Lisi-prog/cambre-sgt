03fe8dc346e9dc095df29c5bfa38f3d4933cb60e
JDCaruza <j.d.caruza@gmail.com>
2024-03-11 11:27:16 -0300

    fix numeroProyecto

--- resources/views/Ingenieria/Servicios/Proyectos/modal/crear-proyecto.blade.php
+++ resources/views/Ingenieria/Servicios/Proyectos/modal/crear-proyecto.blade.php
@@ -157,20 +157,16 @@
                 id: id,
             },
             success: function (response) {
-                console.log(response);
-                
                 if (response) {
                     let numeroDeProy = response.codigo_servicio.substr(e.options[e.selectedIndex].text.length);
-                    let numeroDeProyLengInicial = numeroDeProy.length;
-                    console.log(numeroDeProy);
+                    let numeroDeProyLengInicial;
                     numeroDeProy = numeroDeProy.match(/[0-9]*/g);
+                    numeroDeProyLengInicial = numeroDeProy[0].length //Largo del primer numero encontrado
                     numeroDeProy = String(Number(numeroDeProy[0])+1)
                     while(numeroDeProy.length < numeroDeProyLengInicial){
                         numeroDeProy = '0' + numeroDeProy
                     }
-                    console.log(numeroDeProy);
                     let prefijoMasNumero = e.options[e.selectedIndex].text + numeroDeProy
-                    console.log(prefijoMasNumero);
                     input_codigo_proyecto.value = prefijoMasNumero + response.codigo_servicio.substr(prefijoMasNumero.length);
                 }else{
                     input_codigo_proyecto.value = e.options[e.selectedIndex].text;
