f87f1e877b30f73fb3a6c6e8b3f383d25c0a9f0a
Lisi-prog <80710956+Lisi-prog@users.noreply.github.com>
2024-03-11 11:13:09 -0300

    fix add +1 proyecto

--- resources/views/Ingenieria/Servicios/Proyectos/modal/crear-proyecto.blade.php
+++ resources/views/Ingenieria/Servicios/Proyectos/modal/crear-proyecto.blade.php
@@ -157,20 +157,20 @@
                 id: id,
             },
             success: function (response) {
-                console.log(response);
+                //console.log(response);
                 
                 if (response) {
                     let numeroDeProy = response.codigo_servicio.substr(e.options[e.selectedIndex].text.length);
                     let numeroDeProyLengInicial = numeroDeProy.length;
-                    console.log(numeroDeProy);
+                    //console.log(numeroDeProy);
                     numeroDeProy = numeroDeProy.match(/[0-9]*/g);
                     numeroDeProy = String(Number(numeroDeProy[0])+1)
                     while(numeroDeProy.length < numeroDeProyLengInicial){
                         numeroDeProy = '0' + numeroDeProy
                     }
-                    console.log(numeroDeProy);
+                    //console.log(numeroDeProy);
                     let prefijoMasNumero = e.options[e.selectedIndex].text + numeroDeProy
-                    console.log(prefijoMasNumero);
+                    //console.log(prefijoMasNumero);
                     input_codigo_proyecto.value = prefijoMasNumero + response.codigo_servicio.substr(prefijoMasNumero.length);
                 }else{
                     input_codigo_proyecto.value = e.options[e.selectedIndex].text;
