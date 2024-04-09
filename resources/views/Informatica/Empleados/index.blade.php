3e069859feb1c9c5f5b34a35ec906f4524515ff8
Lisi-prog <80710956+Lisi-prog@users.noreply.github.com>
2024-04-09 17:07:06 -0300

    Update index.blade.php

--- resources/views/Informatica/Empleados/index.blade.php
+++ resources/views/Informatica/Empleados/index.blade.php
@@ -10,15 +10,15 @@
             <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2 my-auto">
                 <h4 class="titulo page__heading my-auto">Técnicos</h5>
             </div>
-            <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2 mx-4">
+            <div class="col-xs-1 col-sm-1 col-md-1 col-lg-1">
                 {!! Form::open(['method' => 'GET', 'route' => ['puesto_tecnico.index'], 'class' => 'd-flex justify-content-end']) !!}
                     {!! Form::submit('Puesto de técnico', ['class' => 'btn btn-success']) !!}
                 {!! Form::close() !!}
             </div>
-            <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+            <div class="col-xs-6 col-sm-6 col-md-6 col-lg-7">
             </div>
             
-            <div class="col-xs-1 col-sm-1 col-md-1 col-lg-1">
+            <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2">
                 {!! Form::open(['method' => 'GET', 'route' => ['tecnicos.create'], 'class' => 'd-flex justify-content-end']) !!}
                     {!! Form::submit('Nuevo', ['class' => 'btn btn-success col-9']) !!}
                 {!! Form::close() !!}
