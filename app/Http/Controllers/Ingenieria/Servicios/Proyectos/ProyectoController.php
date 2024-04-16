c95c91f251cbb56338e4ac1a3c2f4aceb5fb3c7d
Lisi-prog <80710956+Lisi-prog@users.noreply.github.com>
2024-04-16 16:55:02 -0300

    Update ProyectoController.php

--- app/Http/Controllers/Ingenieria/Servicios/Proyectos/ProyectoController.php
+++ app/Http/Controllers/Ingenieria/Servicios/Proyectos/ProyectoController.php
@@ -123,7 +123,7 @@ class ProyectoController extends Controller
                 break;
         }
 
-        $proyectos = Vw_servicio::whereIn('id_servicio', $id_serv)->servicio($request->input('cod_serv'))->tipo($request->input('tipos'))->prefijo($prefijo)->lider($request->input('lid'))->estado($request->input('estados'))->orderBy('prioridad_servicio')->paginate(25);
+        return $proyectos = Vw_servicio::whereIn('id_servicio', $id_serv)->servicio($request->input('cod_serv'))->tipo($request->input('tipos'))->prefijo($prefijo)->lider($request->input('lid'))->estado($request->input('estados'))->orderBy('prioridad_servicio')->paginate(25);
         
         // try {
         //     if (in_array(9, $request->input('estados')) || in_array(10, $request->input('estados')) ) {
