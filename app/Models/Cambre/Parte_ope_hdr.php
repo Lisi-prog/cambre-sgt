a21be3daf492b8a8e2cb9e5f6418ad65eb4147ba
Lisi-prog <80710956+Lisi-prog@users.noreply.github.com>
2025-09-04 19:00:03 -0300

    add js

--- app/Models/Cambre/Parte_ope_hdr.php
+++ app/Models/Cambre/Parte_ope_hdr.php
@@ -26,8 +26,18 @@ class Parte_ope_hdr extends Model
         'id_responsabilidad',
         'horas',
         'id_estado_hdr',
-        'medidas'
+        'medidas',
+        'ruta_cam'
     ];
+
+    // public function getMedidasAttribute()
+    // {
+    //     if ($this->attributes['medidas']) {
+    //         return 'SI';
+    //     } else {
+    //         return 'NO';
+    //     }
+    // }
 
     public function getEstado(){
         return $this->hasOne(Estado_hdr::class, 'id_estado_hdr');
@@ -35,5 +45,18 @@ class Parte_ope_hdr extends Model
 
     public function getNombreEstado(){
         return Estado_hdr::where('id_estado_hdr', $this->id_estado_hdr)->first()->nombre_estado_hdr;
+    }
+
+    public function getResponsable()
+    {
+        return $this->belongsTo(Responsabilidad::class, 'id_responsabilidad');
+    }
+
+    public function getFinalizado()
+    {
+        if ($this->id_estado_hdr == 4) { //orden completa
+            return 1;
+        }
+        return 0;
     }
 }
