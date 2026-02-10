da5f3db4b4f03f67af6af383b73ac6934dbb13a0
Lisi-prog <80710956+Lisi-prog@users.noreply.github.com>
2026-02-09 23:26:16 -0300

    se agrega la opcion de informes

---
+++ app/Http/Controllers/Informatica/InformeController.php
@@ -0,0 +1,185 @@
+<?php
+
+namespace App\Http\Controllers\Informatica;
+use App\Http\Controllers\Controller;
+
+use Illuminate\Http\Request;
+
+//agregamos
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Spatie\Permission\Models\Role;
+
+use App\Models\User;
+use App\Models\Cambre\Empleado;
+use App\Models\Cambre\Og_organigrama;
+// use App\Models\Cambre\Sector;
+// use App\Models\Cambre\Puesto_empleado;
+// use App\Models\Cambre\Em_not_x_empleado;
+// use App\Models\Cambre\Em_notificacion;
+// use App\Models\Cambre\Og_organigrama;
+// use App\Models\Cambre\Maquinaria;
+// use App\Models\Cambre\Emp_x_maq;
+
+class InformeController extends Controller
+{
+    function __construct()
+    {
+        $this->middleware('auth');
+        //  $this->middleware('permission:VER-PERMISO|CREAR-PERMISO|EDITAR-PERMISO|BORRAR-PERMISO', ['only' => ['index']]);
+        //  $this->middleware('permission:CREAR-PERMISO', ['only' => ['create','store']]);
+        //  $this->middleware('permission:EDITAR-PERMISO', ['only' => ['edit','update']]);
+        //  $this->middleware('permission:BORRAR-PERMISO', ['only' => ['destroy']]);
+    }
+    
+    public function index(Request $request)
+    {        
+        $tipos = [
+            1 => "Resumen Semanal"
+        ];
+
+        $supervisores = $this->obtenerSupervisores();
+        return view('Informatica.Informes.index',compact('tipos', 'supervisores'));
+    }
+
+    public function create()
+    {
+        
+    }
+
+    public function store(Request $request)
+    {
+        $opcion = $request->input('id_informe');
+        $fechaIni = $request->input('fecha_desde');
+        $fechaFin = $request->input('fecha_hasta');
+        $supervisor = $request->input('supervi');
+
+        switch ($opcion) {
+            case 1:
+                return $this->generarResumenSemanal($fechaIni, $fechaFin, $supervisor);
+                break;
+        }
+        return $request;                     
+    }
+    
+    public function show($id)
+    {
+    }
+    
+    public function edit($id)
+    {
+       
+    }
+
+    public function update(Request $request, $id)
+    {
+                                
+    }
+    
+    public function destroy($id)
+    {
+                      
+    }
+
+    public function generarResumenSemanal($fechaIni, $fechaFin, $supervisor)
+    {
+        $fechaHoy = $fechaFin;
+        $fechaHace7Dias = $fechaIni;
+
+        $empleadoId = $supervisor;
+
+        // Obtener datos de servicios del usuario
+        $datosUsuario = DB::select('CALL ObtenerTotalHorasServicio(?, ?, ?)', [$empleadoId, $fechaHace7Dias, $fechaHoy]);
+
+        if (count($datosUsuario) != 0) {
+            // Preparar datos para el gráfico del usuario
+            $datosGraficoUsuario = $this->prepararDatosGrafico($datosUsuario);
+
+            // Obtener lista de subordinados
+            $subordinados = Og_organigrama::where('id_supervisor_directo', $empleadoId)->pluck('id_empleado')->toArray();
+
+            // Obtener datos de subordinados de forma masiva
+            $empleados = Empleado::whereIn('id_empleado', $subordinados)->pluck('nombre_empleado', 'id_empleado');
+            $datosSubordinados = [];
+
+            foreach ($subordinados as $subId) {
+                $datosSub = DB::select('CALL ObtenerTotalHorasServicio(?, ?, ?)', [$subId, $fechaHace7Dias, $fechaHoy]);
+                $nombreEmpleado = $empleados[$subId] ?? 'Desconocido';
+
+                // Preparar datos para el gráfico del subordinado
+                $datosGraficoSub = $this->prepararDatosGrafico($datosSub);
+                $totalHorasSub = isset($datosSub[0]) ? substr($datosSub[0]->total_ac, 0, -3) : '0';
+
+                $datosSubordinados[] = [
+                    'name' => $nombreEmpleado,
+                    'info' => $datosSub,
+                    'total_horas' => $totalHorasSub,
+                    'chart' => $datosGraficoSub['chart_url']
+                ];
+            }
+
+            // Preparar datos para el correo
+            $data = [
+                'info' => $datosUsuario,
+                'fecha_desde' => $fechaHace7Dias,
+                'fecha_hasta' => $fechaHoy,
+                'total_horas' => substr($datosUsuario[0]->total_ac, 0, -3),
+                'chart' => $datosGraficoUsuario['chart_url'],
+                'datos_sub' => $datosSubordinados
+            ];
+
+            return [
+                'data' => $data,
+                'vacio' => 0
+            ];
+        } else {
+            return [
+                'vacio' => 1
+            ];
+        }
+    }
+
+    private function prepararDatosGrafico($datos)
+    {
+        $codigo = [];
+        $porcentaje = [];
+
+        foreach ($datos as $item) {
+            $codigo[] = $item->codigo_servicio;
+            $porcentaje[] = $item->porcentaje;
+        }
+
+        if (empty($codigo) || empty($porcentaje)) {
+            return ['chart_url' => ''];
+        }
+
+        $chartData = [
+            'type' => 'pie',
+            'data' => [
+                'labels' => $codigo,
+                'datasets' => [['data' => $porcentaje]]
+            ],
+            'options' => [
+                'plugins' => [
+                    'datalabels' => [
+                        'display' => true,
+                        'align' => 'bottom',
+                        'backgroundColor' => '#ccc',
+                        'borderRadius' => 3,
+                        'font' => ['size' => 18],
+                    ]
+                ]
+            ]
+        ];
+
+        $chartData = json_encode($chartData);
+        $chartURL = "https://quickchart.io/chart?width=600&height=400&c=" . urlencode($chartData);
+        
+        return ['chart_url' => $chartURL];
+    }
+
+    public function obtenerSupervisores(){
+        $usuariosSupervisor = User::role('SUPERVISOR')->pluck('id')->toArray();
+        return Empleado::whereIn('user_id', $usuariosSupervisor)->orderBy('nombre_empleado')->pluck('nombre_empleado', 'id_empleado');
+    }
+}
---
+++ resources/views/Informatica/Informes/index.blade.php
@@ -0,0 +1,209 @@
+@extends('layouts.app')
+@section('titulo', 'Informes')
+
+@section('content')
+<style>
+
+</style>
+<section class="section">
+    <div class="d-flex section-header justify-content-center">
+        <div class="d-flex flex-row col-12">
+            <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4 my-auto">
+                <h4 class="titulo page__heading my-auto">Informes</h5>
+            </div>
+            <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+            </div>
+            <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2 mx-4">
+            </div>
+        </div>
+    </div>
+    {{-- @include('layouts.modal.mensajes', ['modo' => 'Agregar']) --}}
+    <div class="section-body">   
+        <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                <div class="card">
+                    <div class="card-body">
+                        {!! Form::open(['route' => 'informes.store', 'method' => 'POST', 'class' => 'formulario', 'id' => 'formulario_informe']) !!}
+                        <div class="row">
+                            <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2">
+                                <div class="form-group">
+                                    {!! Form::label('informe', 'Informe:', ['class' => 'control-label fs-7', 'style' => 'white-space: nowrap; ']) !!}
+                                    {!! Form::select('id_informe', $tipos, null, [
+                                            'placeholder' => 'Seleccionar',
+                                            'class' => 'form-select',
+                                            'id' => 'id_informe',
+                                            'required'
+                                        ]) !!}
+                                </div>
+                            </div>
+                            <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2">
+                                <div class="form-group">
+                                    {!! Form::label('fec_desde', 'Fecha Desde:', ['class' => 'control-label fs-7', 'style' => 'white-space: nowrap; ']) !!}
+                                    {!! Form::date('fecha_desde', \Carbon\Carbon::now(), [
+                                        'min' => '2023-01-01',
+                                        'max' => \Carbon\Carbon::now()->year . '-12',
+                                        'id' => 'input-fec_ini',
+                                        'class' => 'form-control',
+                                        'required'
+                                    ]) !!}
+                                </div>
+                            </div>
+                            <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2">
+                                <div class="form-group">
+                                    {!! Form::label('fec_hasta', 'Fecha Hasta:', ['class' => 'control-label fs-7', 'style' => 'white-space: nowrap; ']) !!}
+                                    {!! Form::date('fecha_hasta', \Carbon\Carbon::now(), [
+                                        'min' => '2023-01-01',
+                                        'max' => \Carbon\Carbon::now()->year . '-12',
+                                        'id' => 'input-fec_ini',
+                                        'class' => 'form-control',
+                                        'required'
+                                    ]) !!}
+                                </div>
+                            </div>
+                            <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2">
+                                <div class="form-group">
+                                    {!! Form::label('supervi', 'Supervisor:', ['class' => 'control-label fs-7', 'style' => 'white-space: nowrap; ']) !!}
+                                    {!! Form::select('supervi', $supervisores, null, [
+                                            'placeholder' => 'Seleccionar',
+                                            'class' => 'form-select',
+                                            'id' => 'id_supervi',
+                                            'required'
+                                        ]) !!}
+                                </div>
+                            </div>
+                            <div class="col-xs-1 col-sm-1 col-md-1 col-lg-1 my-auto">
+                                <div class="form-group my-auto">
+                                    {!! Form::submit('Generar', ['class' => 'btn btn-success w-100']) !!}
+                                </div>
+                            </div>
+                        </div>
+                        {!! Form::close() !!}
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12" id="ver-informes" hidden>
+                <div class="card">
+                    <div class="card-body">
+                        @include('layouts.loanding')
+                        <div class="row">
+                            <div class="col-xs-10 col-sm-10 col-md-10 col-lg-10">
+                            </div>
+                            <div class="col-xs-1 col-sm-1 col-md-1 col-lg-1">
+                            </div>
+                            <div class="col-xs-1 col-sm-1 col-md-1 col-lg-1">
+                                <button class="btn btn-warning w-100">PDF</button>
+                            </div>
+                        </div>
+                        <div class="row" id="vista-grafico">
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    <script>
+        $(document).ready(function () {
+            document.getElementById("formulario_informe").addEventListener("submit", function(e) {
+                e.preventDefault();
+
+                var url_php = $(this).attr("action"); 
+                var type_method = $(this).attr("method"); 
+                var form_data = $(this).serialize();
+            
+                $("#loading").show();
+                $.ajax({
+                    type: type_method,
+                    url: url_php,
+                    data: form_data,
+                    timeout: 60000, // 60 segundos de espera
+                    success: function(res) {
+                        document.getElementById('ver-informes').hidden = false;
+                        let trProyAv = '';
+                        let trEmpAv = '';
+                        console.log(res);
+                        let grafico = `<div class="" style="width: 30vw; height: 20vw; position: relative;">
+                                            <img src="${res.data.chart}" style="width: 100%; height: 100%; object-fit: cover; object-position: center;">
+                                        </div>`;
+
+                        res.data.info
+                        .sort((a, b) => b.porcentaje - a.porcentaje)
+                        .forEach(e => {
+                            trProyAv += `<tr style="">
+                                            <td class='text-end' style="vertical-align: middle; border: 1px solid #000; border-spacing: 0; width: 25%;">${e.codigo_servicio}</td>
+                                                                                
+                                            <td class='text-center' style="vertical-align: middle; border: 1px solid #000; border-spacing: 0; width: 25%;">${e.h_total}</td>
+                                                                                
+                                            <td class='text-center' style="vertical-align: middle; border: 1px solid #000; border-spacing: 0; width: 25%;">${e.porcentaje}%</td>
+                                                                                
+                                        </tr>`;
+                        });
+
+                        res.data.datos_sub
+                        .sort((a, b) => a.name.localeCompare(b.name))
+                        .forEach(e => {
+                            trEmpAv += `<tr style="">
+                                            <td class='text-end' style="vertical-align: middle; border: 1px solid #000; border-spacing: 0; width: 25%;">${e.name}</td>
+                                                                                
+                                            <td class='text-center' style="vertical-align: middle; border: 1px solid #000; border-spacing: 0; width: 25%;">${e.total_horas}</td>
+                                                                                
+                                        </tr>`;
+                        });
+
+                        document.getElementById('vista-grafico').innerHTML = `
+                                                                                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 text-center">
+                                                                                    <h3>Total Hs: ${res.data.total_horas}</h3>
+                                                                                </div>
+                                                                                <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+                                                                                    <label class="control-label fs-7" style="white-space: nowrap; ">Grafico:</label>
+                                                                                    ${grafico}
+                                                                                </div>
+                                                                                <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+                                                                                    <label class="control-label fs-7" style="white-space: nowrap; ">Avance Proyectos:</label>
+                                                                                    <table>
+                                                                                        <thead style="height:50px;">
+                                                                                            <th class='ml-3 text-center' style="color:#000; border: 1px solid #000; border-spacing: 0; width: 25%;">Proyecto</th>
+                                                                                            <th class='text-center' style="color:#000; border: 1px solid #000; border-spacing: 0; width: 25%;">Horas</th>
+                                                                                            <th class='text-center' style="color:#000; border: 1px solid #000; border-spacing: 0; width: 25%;">Porcentaje</th>
+                                                                                        </thead>
+                                                                                        <tbody>
+                                                                                            ${trProyAv}
+                                                                                        </tbody>
+                                                                                    </table>
+                                                                                </div>
+                                                                                <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+                                                                                    <label class="control-label fs-7" style="white-space: nowrap; ">Avance Proyectos:</label>
+                                                                                    <table>
+                                                                                        <thead style="height:50px;">
+                                                                                            <th class='ml-3 text-center' style="color:#000; border: 1px solid #000; border-spacing: 0; width: 25%;">Empleado</th>
+                                                                                            <th class='text-center' style="color:#000; border: 1px solid #000; border-spacing: 0; width: 25%;">Horas</th>
+                                                                                        </thead>
+                                                                                        <tbody>
+                                                                                            ${trEmpAv}
+                                                                                        </tbody>
+                                                                                    </table>
+                                                                                </div>
+                                                                            `;
+                    },
+                    complete: function() {
+                        $("#loading").hide(); 
+                    },
+                    error: function(jqXHR, textStatus) {
+                        $("#loading").hide();
+                        if (textStatus === "timeout") {
+                            alert("La generacion del informe tardo demaciado.");
+                        } else {
+                            alert("Error en la solicitud: " + textStatus);
+                        }
+                    }
+                });
+            });
+        });
+    </script>
+</section>
+
+@endsection
